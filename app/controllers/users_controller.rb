9679f3259f262e4bfae359eb448e93e34a2f21da
Christopher Carrasco <chris@Christophers-MacBook-Pro.local>
2018-06-02 18:46:37 -0400

    changing :index to :show removing restriction while developing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: [:index, :destroy]
+  before_action :authenticate_user, only: [:show, :destroy]
 
   def index
       render json:  User.all.to_json
